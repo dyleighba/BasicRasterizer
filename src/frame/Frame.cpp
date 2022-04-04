12c5201c58bbeb7e1e7f99bb7ffecf0376b76612
Dylan Barber <dylan@dlbarber.dev>
2022-04-05 03:41:23 +0800

    Implemented Frame class

--- src/frame/Frame.cpp
+++ src/frame/Frame.cpp
@@ -3,3 +3,25 @@
 //
 
 #include "Frame.h"
+
+int Frame::xyToIndex(int x, int y) {
+    return (width * y) + x;
+}
+
+Frame::Frame(int width, int height) {
+    this->width = width;
+    this->height = height;
+    pixels[xyToIndex(width, height)];
+}
+
+void Frame::setPixel(int x, int y, linalg::vec<float, 3> rgb) {
+    int index = xyToIndex(x, y);
+    pixels[index] = rgb;
+}
+
+linalg::vec<float, 3> Frame::getPixel(int x, int y) {
+    int index = xyToIndex(x, y);
+    return pixels[index];
+}
+
+
--- src/frame/Frame.h
+++ src/frame/Frame.h
@@ -5,9 +5,18 @@
 #ifndef BASICRASTERIZER_FRAME_H
 #define BASICRASTERIZER_FRAME_H
 
+#include <linalg.h>
 
 class Frame {
+    int width;
+    int height;
+    linalg::vec<float,3> pixels[];
 
+    int xyToIndex(int x, int y);
+public:
+    Frame(int width, int height);
+    void setPixel(int x, int y, linalg::vec<float,3> rgb);
+    linalg::vec<float,3> getPixel(int x, int y);
 };
 
 
