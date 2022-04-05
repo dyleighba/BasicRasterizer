ed14c1166a26627d407a166ed383fc91380f7430
Dylan Barber <dylan@dlbarber.dev>
2022-04-05 20:10:45 +0800

    Added Rect struct

---
+++ src/draw/Rect.h
@@ -0,0 +1,15 @@
+//
+// Created by dylan on 5/04/2022.
+//
+
+#ifndef BASICRASTERIZER_RECT_H
+#define BASICRASTERIZER_RECT_H
+
+struct Rect {
+    float x = 0;
+    float y = 0;
+    float width = 0;
+    float height = 0;
+};
+
+#endif //BASICRASTERIZER_RECT_H
