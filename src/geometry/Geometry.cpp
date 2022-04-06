2585d1d69cbea6916153287898f1ac11b5cb39ac
Dylan Barber <dylan@dlbarber.dev>
2022-04-07 04:14:45 +0800

    Implemented Geometry class

--- src/geometry/Geometry.cpp
+++ src/geometry/Geometry.cpp
@@ -3,3 +3,38 @@
 //
 
 #include "Geometry.h"
+
+Geometry::Geometry(int triangleAmount) {
+    this->triangleAmount = triangleAmount;
+    triangles = new float3[triangleAmount*3];
+    colours = new Colour[triangleAmount];
+}
+
+Geometry::~Geometry() {
+    delete(triangles);
+    delete(colours);
+}
+
+void Geometry::setTriangle(int index, Triangle3D triangle) {
+    int realIndex = index * 3;
+    triangles[realIndex] = triangle.a;
+    triangles[realIndex+1] = triangle.b;
+    triangles[realIndex+2] = triangle.c;
+}
+
+Triangle3D Geometry::getTriangle(int index) {
+    int realIndex = index * 3;
+    return Triangle3D{
+        triangles[realIndex],
+        triangles[realIndex+1],
+        triangles[realIndex+2]
+    };
+}
+
+void Geometry::setTriangleColour(int index, Colour colour) {
+    colours[index] = colour;
+}
+
+Colour Geometry::getTriangleColour(int index) {
+    return colours[index];
+}
--- src/geometry/Geometry.h
+++ src/geometry/Geometry.h
@@ -5,9 +5,25 @@
 #ifndef BASICRASTERIZER_GEOMETRY_H
 #define BASICRASTERIZER_GEOMETRY_H
 
+#include <geometry/Triangle3D.h>
+#include <draw/Colour.h>
+#include <linalg.h>
+
+
+
+using linalg::aliases::float3;
 
 class Geometry {
-
+    int triangleAmount;
+    float3 *triangles;
+    Colour *colours;
+public:
+    explicit Geometry(int triangleAmount);
+    ~Geometry();
+    void setTriangle(int index, Triangle3D triangle);
+    Triangle3D getTriangle(int index);
+    void setTriangleColour(int index, Colour colour);
+    Colour getTriangleColour(int index);
 };
 
 
